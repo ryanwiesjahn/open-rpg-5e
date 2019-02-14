acbf13a2771f87838ea1a3e773d81dd4423c747a
Ryan Wiesjahn <ryanwiesjahn@gmail.com>
2019-02-14 11:38:05 -0500

    Better typing for SpellTrait

--- src/interfaces/Traits/SpellTrait.ts
+++ src/interfaces/Traits/SpellTrait.ts
@@ -4,16 +4,15 @@ import * as Customization from '../Customization'
 
 export type SpellTraitType = Types.SelectTypes.Choose | Types.SelectTypes.Filter
 
-interface SpellTrait<TTraitType extends SpellTraitType> {
+export type SpellTrait<TTraitType extends SpellTraitType> = {
   traitType: TTraitType
   customization?: Customization.SpellCustomization
   isInfinite?: boolean
-}
-
-export interface ChooseSpellTrait extends SpellTrait<Types.SelectTypes.Choose> {
+} 
+& TTraitType extends Types.SelectTypes.Choose ? {
   choiceIds?: UUID[]
 }
-
-export interface FilterCreatureTrait extends SpellTrait<Types.SelectTypes.Filter> {
+: TTraitType extends Types.SelectTypes.Filter ? {
   filter: Filters.SpellFilter
 }
+: null
